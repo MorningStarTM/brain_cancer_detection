66eeef855408c2f1c6e4d0b085100a46d7a0e4a5
ErnestBeckham <ernestbeckham1998@gmail.com>
2023-04-16 17:12:26 +0530

    commit

--- app.py
+++ app.py
@@ -1,22 +1,38 @@
 from flask import Flask, render_template, url_for, request
-from PIL import Image
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+import numpy as np
+
+
+class_names = ["glioma", "meningioma", "no-tumor", "pituitary"]
+model = load_model("./assets/brain_tumor_model_1.h5")
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            return 'No file uploaded!'
-        
-        file = request.files['file']
-        
-        # Preprocess image for prediction
-        img = Image.open(file.stream)
-        img = img.resize((224, 224))
-        print(file)
+@app.route('/', methods=['GET'])
+def index():        
     return render_template('index.html')
 
+@app.route('/', methods=['POST'])
+def predict():
+    imagefile = request.files['imagefile']
+    image_path = "./images/" + imagefile.filename
+    imagefile.save(image_path)
+    
+    img = load_img(image_path, target_size=(224,224))
+    img = img_to_array(img)
+    image = img / 255.0
+    image = image.astype(np.float32)
+    image = image.reshape(1, 224, 224, 3)
+    prediction = model.predict(image)
+    final = np.argmax(prediction)
+    classi = class_names[final]
+    print(classi)
+
+
+    return render_template('index.html', classi=classi)
 
 
 if __name__ == '__main__':
